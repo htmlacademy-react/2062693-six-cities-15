7a318c4c07446607f804a70499f07ac48ccece62
vinokurov <239@b2b-logist.com>
2024-02-17 08:47:41 +0500

    Исправлены отступы

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -84,10 +84,10 @@ export default function Main ({placesFound}: PropTypes): JSX.Element {
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex={0}>
                 Popular
-                <svg className="places__sorting-arrow" width="7" height="4">
-                  <use xlinkHref="#icon-arrow-select"></use>
-                </svg>
-              </span>
+                  <svg className="places__sorting-arrow" width="7" height="4">
+                    <use xlinkHref="#icon-arrow-select"></use>
+                  </svg>
+                </span>
                 <ul className="places__options places__options--custom places__options--opened">
                   <li className="places__option places__option--active" tabIndex={0}>Popular</li>
                   <li className="places__option" tabIndex={0}>Price: low to high</li>
