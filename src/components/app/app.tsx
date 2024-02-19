0c0bd1da48abe975c38cfc081483ae2341909864
vinokurov <239@b2b-logist.com>
2024-02-19 22:32:10 +0500

    Пути маршрутов вынесены в константы

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,6 +5,7 @@ import Login from '../../pages/login/login.tsx';
 import Favorites from '../../pages/favorites/favorites.tsx';
 import Offer from '../../pages/offer/offer.tsx';
 import NotFound404 from '../../pages/404/not-found-404.tsx';
+import {AppRoute} from '../../const.ts';
 
 const placesFound: number = 95;
 
@@ -12,13 +13,11 @@ export default function App (): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={'/'}>
-          <Route index element={<Main placesFound={placesFound}/>} />
-          <Route path={'login'} element={<Login />} />
-          <Route path={'favorites'} element={<Favorites />} />
-          <Route path={'offer/:id'} element={<Offer />} />
-        </Route>
-        <Route path={'*'} element={<NotFound404 />} />
+        <Route path={AppRoute.MAIN} element={<Main placesFound={placesFound}/>} />
+        <Route path={AppRoute.LOGIN} element={<Login />} />
+        <Route path={AppRoute.FAVORITES} element={<Favorites />} />
+        <Route path={AppRoute.OFFER} element={<Offer />} />
+        <Route path={AppRoute.NOT_FOUND} element={<NotFound404 />} />
       </Routes>
     </BrowserRouter>
   );
