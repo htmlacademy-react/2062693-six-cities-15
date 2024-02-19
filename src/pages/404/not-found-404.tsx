b2098482dace214bd032dbaeb0b415e766c94a0c
vinokurov <239@b2b-logist.com>
2024-02-19 22:18:15 +0500

    Добавлена ссылка на главную страницу

--- src/pages/404/not-found-404.tsx
+++ src/pages/404/not-found-404.tsx
@@ -1,7 +1,11 @@
 import {JSX} from 'react';
+import {Link} from 'react-router-dom';
 
 export default function NotFound404(): JSX.Element {
   return (
-    <h1>404 Not Found</h1>
+    <>
+      <h1>404 Not Found</h1>
+      <Link to={'/'} >На главную страницу</Link>
+    </>
   );
 }
