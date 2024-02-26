2523758cc312801148bff3fe7f8ad5f458d251fa
vinokurov <239@b2b-logist.com>
2024-02-26 20:23:58 +0500

    Добавлено описание типа и mock для объявления

---
+++ src/mocks/offers.ts
@@ -0,0 +1,39 @@
+import {Offer} from '../types/offer.ts';
+
+export const offers: Offer[] = [
+  {
+    id: 1,
+    name: 'Beautiful &amp; luxurious apartment at great location',
+    image: 'img/apartment-01.jpg',
+    price: 120,
+    type: 'Apartment'
+  },
+  {
+    id: 2,
+    name: 'Wood and stone place',
+    image: 'img/room.jpg',
+    price: 80,
+    type: 'Room'
+  },
+  {
+    id: 3,
+    name: 'Canal View Prinsengracht',
+    image: 'img/apartment-02.jpg',
+    price: 132,
+    type: 'Apartment'
+  },
+  {
+    id: 4,
+    name: 'Nice, cozy, warm big bed apartment',
+    image: 'img/apartment-03.jpg',
+    price: 180,
+    type: 'Apartment'
+  },
+  {
+    id: 5,
+    name: 'Wood and stone place',
+    image: 'img/room.jpg',
+    price: 80,
+    type: 'Room'
+  }
+];
---
+++ src/types/offer.ts
@@ -0,0 +1,7 @@
+export type Offer = {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  type: 'Room' | 'Apartment';
+};
