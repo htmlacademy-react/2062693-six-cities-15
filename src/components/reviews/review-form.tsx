07a5a878bce84c182cafc13dbbf5ba548a27ceff
vinokurov <239@b2b-logist.com>
2024-03-09 16:55:20 +0500

    Форматирование строк

--- src/components/reviews/review-form.tsx
+++ src/components/reviews/review-form.tsx
@@ -7,30 +7,28 @@ export default function ReviewForm() {
   const [submitDisabled, setSubmitDisabled] = useState(true);
 
   const setRating = (newRating: boolean[]) => {
-    setInputFields(prevState => {
+    setInputFields((prevState) => {
       return {...prevState, rating: newRating};
     });
   };
 
   const onChangeMessageValue = (evt: ChangeEvent<HTMLTextAreaElement>) => {
-    setInputFields(prevState => ({...prevState, message: evt.target.value}))
+    setInputFields((prevState) => ({...prevState, message: evt.target.value}));
   };
 
   const submitForm = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
   };
 
-    useEffect(() => {
-      setSubmitDisabled(inputFields.message.length < 50 || !inputFields.rating.includes(true));
-    }, [inputFields]);
+  useEffect(() => {
+    setSubmitDisabled(inputFields.message.length < 50 || !inputFields.rating.includes(true));
+  }, [inputFields]);
 
   return (
     <form className="reviews__form form" method="post" onSubmit={submitForm}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <ReviewRatingStars rating={inputFields.rating} setRating={setRating}/>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review"
-                placeholder="Tell how was your stay, what you like and what can be improved" value={inputFields.message}
-                onChange={onChangeMessageValue}></textarea>
+      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" value={inputFields.message} onChange={onChangeMessageValue}></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe
