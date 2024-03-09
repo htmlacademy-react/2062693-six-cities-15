84bcdccf978f733802f0d90837879a8f9b002af1
vinokurov <239@b2b-logist.com>
2024-03-09 09:48:09 +0500

    Обновляет передачу props

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,21 +3,28 @@ import Main from '../../pages/main/main.tsx';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from '../../pages/login/login.tsx';
 import Favorites from '../../pages/favorites/favorites.tsx';
-import Offer from '../../pages/offer/offer.tsx';
 import NotFound404 from '../../pages/404/not-found-404.tsx';
 import {AppRoute, AuthorizationStatus} from '../../const.ts';
 import PrivateRoute from '../private-route/private-route.tsx';
+import {Review} from '../../types/review.ts';
+import {OfferType} from '../../types/offer.ts';
+import Offer from '../../pages/offer/offer.tsx';
+
+type AppPropTypes = {
+  offers: OfferType[];
+  reviews: Review[];
+};
 
 const placesFound: number = 95;
 
-export default function App (): JSX.Element {
+export default function App ({offers, reviews}: AppPropTypes): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Main} element={<Main placesFound={placesFound}/>} />
+        <Route path={AppRoute.Main} element={<Main placesFound={placesFound} offers={offers}/>} />
         <Route path={AppRoute.Login} element={<Login />} />
-        <Route path={AppRoute.Favorites} element={<PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}><Favorites /></PrivateRoute>} />
-        <Route path={AppRoute.Offer} element={<Offer />} />
+        <Route path={AppRoute.Favorites} element={<PrivateRoute authorizationStatus={AuthorizationStatus.Auth}><Favorites offers={offers} /></PrivateRoute>} />
+        <Route path={AppRoute.Offer} element={<Offer reviews={reviews} offers={offers} authorizationStatus={AuthorizationStatus.Auth}/>} />
         <Route path={AppRoute.NotFound} element={<NotFound404 />} />
       </Routes>
     </BrowserRouter>
