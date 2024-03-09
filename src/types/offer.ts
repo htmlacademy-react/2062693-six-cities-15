2dbdaae665f2ce78f3d4f7751447879f404cb887
vinokurov <239@b2b-logist.com>
2024-03-09 09:45:14 +0500

    Дополняет тип OfferType

--- src/types/offer.ts
+++ src/types/offer.ts
@@ -1,8 +1,10 @@
-export type Offer = {
+export type OfferType = {
   id: number;
   name: string;
   image: string;
+  smallImage: string;
   price: number;
   type: 'Room' | 'Apartment';
   rating: number;
+  city: string;
 };
