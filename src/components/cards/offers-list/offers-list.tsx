1801efa42f8da0a1b06114b43b7237a652bfce1b
vinokurov <239@b2b-logist.com>
2024-03-09 09:48:29 +0500

    Добавляет компоненту списка предложений

---
+++ src/components/cards/offers-list/offers-list.tsx
@@ -0,0 +1,21 @@
+import {PropsWithChildren, useState} from 'react';
+import CityCard from '../city-card/city-card.tsx';
+import {OfferType} from '../../../types/offer.ts';
+
+type TOffersList = PropsWithChildren<{ offers: OfferType[] }>
+
+export default function OffersList({offers}: TOffersList) {
+
+  const [activeOffer, setActiveOffer] = useState(0);
+
+  const changeActiveElementHandler = (activeId: number) => {
+    setActiveOffer(activeId);
+  };
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {offers.map((offer) => (
+        <CityCard key={offer.id} offer={offer} setActive={changeActiveElementHandler}/>
+      ))}
+    </div>
+  );
+}
