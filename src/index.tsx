d57c3f72f719a4a651d460ec618f74da69d60880
vinokurov <239@b2b-logist.com>
2024-03-09 09:45:40 +0500

    Передает список предложений в props

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app.tsx';
+import {offers} from './mocks/offers.ts';
+import {reviews} from './mocks/review.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App offers={offers} reviews={reviews}/>
   </React.StrictMode>
 );
