2a13d8817b548ba695b818f93c919b847b03cf54
vinokurov <239@b2b-logist.com>
2024-03-09 09:48:58 +0500

    Добавляет компоненту списка избранных мест и карточек избранного

---
+++ src/components/favorite/favorite-places-list/favorite-places-list.tsx
@@ -0,0 +1,34 @@
+import {OfferType} from '../../../types/offer.ts';
+import FavoriteCard from '../favorite-card/favorite-card.tsx';
+
+type TFavoritePlacesList = {
+  offers: OfferType[];
+};
+
+export default function FavoritePlacesList({offers}: TFavoritePlacesList) {
+
+  const favoritePlaces = offers.reduce<string[]>((acc, offer) => {
+    if (acc.includes(offer.city)) {
+      return acc;
+    }
+    return [...acc, offer.city];
+  }, []);
+
+  return (
+    <ul className="favorites__list">
+      {favoritePlaces.map((item) => (
+        <li key={item} className="favorites__locations-items">
+          <div className="favorites__locations locations locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#">
+                <span>{item}</span>
+              </a>
+            </div>
+          </div>
+          <div className="favorites__places">
+            {offers.filter((offer) => offer.city === item).map((offer) => (<FavoriteCard key={offer.id} offer={offer}/>))}
+          </div>
+        </li>))}
+    </ul>
+  );
+}
