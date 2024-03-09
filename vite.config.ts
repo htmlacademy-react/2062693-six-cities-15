2e8bd065ab402c8ea7f01c30843fd46a1812359c
vinokurov <239@b2b-logist.com>
2024-03-09 09:44:45 +0500

    Добавляет возможность использования process

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 /// <reference types='vitest' />
 /// <reference types='vite/client' />
 
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  },
+export default defineConfig(({mode}) => {
+  return {
+    define: {
+      'process.env': loadEnv(mode, process.cwd(), '')
+    },
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/setupTests.ts'],
+    },
+  };
 });
