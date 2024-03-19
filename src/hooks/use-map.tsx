97eacb123f68421bed95a7a3cfdaf25fc0adbf22
vinokurov <239@b2b-logist.com>
2024-03-19 20:25:29 +0500

    Изменено условие

--- src/hooks/use-map.tsx
+++ src/hooks/use-map.tsx
@@ -7,7 +7,7 @@ export default function useMap(mapRef: MutableRefObject<HTMLElement | null>, loc
   const isRenderedRef = useRef<boolean>(false);
 
   useEffect(() => {
-    if (mapRef.current !== null && !isRenderedRef.current) {
+    if (mapRef.current && !isRenderedRef.current) {
       const instance = new Map(mapRef.current, {
         center: {
           lat: location.latitude,
