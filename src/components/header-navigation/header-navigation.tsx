d3f2b101b8482e26659f61b3ef86f5da00ca4782
vinokurov <239@b2b-logist.com>
2024-03-31 17:49:15 +0500

    Отображение в шапке состояния авторизации

---
+++ src/components/header-navigation/header-navigation.tsx
@@ -0,0 +1,36 @@
+import {useAppSelector} from '../../hooks';
+import {AppRoute, AuthorizationStatus} from '../../const.ts';
+import {Link} from 'react-router-dom';
+
+export function HeaderNavigation() {
+
+  const authStatus = useAppSelector((state) => state.authorizationStatus);
+  return (
+    <nav className="header__nav">
+      <ul className="header__nav-list">
+        <li className="header__nav-item user">
+          <a className="header__nav-link header__nav-link--profile" href="#">
+            <div className="header__avatar-wrapper user__avatar-wrapper">
+            </div>
+            <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+            <span className="header__favorite-count">3</span>
+          </a>
+        </li>
+        {authStatus === AuthorizationStatus.Auth &&
+          <li className="header__nav-item">
+            <a className="header__nav-link" href="#">
+              <span className="header__signout">Sign out</span>
+            </a>
+          </li>}
+        {authStatus !== AuthorizationStatus.Auth &&
+          <li className="header__nav-item user">
+            <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
+              <div className="header__avatar-wrapper user__avatar-wrapper">
+              </div>
+              <span className="header__login">Sign in</span>
+            </Link>
+          </li>}
+      </ul>
+    </nav>
+  );
+}
